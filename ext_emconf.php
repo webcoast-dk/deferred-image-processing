87322782a91c2435fd5e5bda4af02527fc491b33
Thorben Nissen <thorben@webcoast.dk>
2023-05-19 13:46:27 +0200

    [TASK] Allow installation in TYPO3 CMS 12

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '1.1.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.0-11.5.99'
+            'typo3' => '10.4.0-12.4.99'
         ),
         'conflicts' => array(),
         'suggests' => array(),
