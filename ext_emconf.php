f60fe9ee1dbc97b1737388478d4940566175f79c
Marcus Förster <m.foerster@xerc.de>
2023-09-20 15:08:19 +0200

    [CLEANUP] EXT base-files

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,17 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Deferred image processing',
     'description' => 'Handles image processing on request instead of during page generation',
     'category' => 'plugin',
+    'state' => 'stable',
+    'version' => '2.0.0',
     'author' => 'Thorben Nissen',
     'author_email' => 'thorben@webcoast.dk',
     'author_company' => 'WEBcoast',
-    'state' => 'stable',
-    'clearCacheOnLoad' => 0,
-    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
+            'php' => '7.4.0-8.99.99',
             'typo3' => '11.5.0-12.4.99'
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('TYPO3') || die;
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors'][WEBcoast\DeferredImageProcessing\Resource\Processing\DeferredImageProcessor::class] = [
     'className' => WEBcoast\DeferredImageProcessing\Resource\Processing\DeferredImageProcessor::class,
     'before' => ['LocalImageProcessor'],
