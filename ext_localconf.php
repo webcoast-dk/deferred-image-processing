571c1a09e075da95d6bbc0a5e7a99e29d1bc6be3
Thorben Nissen <github@kapp-hamburg.de>
2023-09-21 10:24:21 +0200

    [TASK] Add message to `die()`

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3') || die;
+defined('TYPO3') || die('Access denied! Cannot run outside of TYPO3 context!');
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['processors'][WEBcoast\DeferredImageProcessing\Resource\Processing\DeferredImageProcessor::class] = [
     'className' => WEBcoast\DeferredImageProcessing\Resource\Processing\DeferredImageProcessor::class,
