aeb8179b6a019b533fb9ab3e86b1c842f2fc2aff
Marcus Förster <m.foerster@xerc.de>
2025-04-27 18:01:22 +0200

    [REV] no defer @ BE login

--- Classes/Resource/Processing/DeferredImageProcessor.php
+++ Classes/Resource/Processing/DeferredImageProcessor.php
@@ -118,6 +118,6 @@ class DeferredImageProcessor extends LocalImageProcessor
             return false;
         }
 
-        return !$context->getAspect('backend.user')->isLoggedIn();
+        return true;#!$context->getAspect('backend.user')->isLoggedIn();
     }
 }
