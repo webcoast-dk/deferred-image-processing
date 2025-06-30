fb7ea805275711064eea7bea97f73b15c523bda4
Thorben Nissen <thorben@webcoast.dk>
2025-06-30 21:14:21 +0200

    [FEATURE] Add `ShouldDeferEvent`

---
+++ Classes/Event/ShouldDeferEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace WEBcoast\DeferredImageProcessing\Event;
+
+
+use TYPO3\CMS\Core\Resource\Processing\TaskInterface;
+
+class ShouldDeferEvent
+{
+    protected bool $shouldDefer = false;
+
+    protected TaskInterface $task;
+
+    public function __construct(bool $shouldDefer, TaskInterface $task)
+    {
+        $this->shouldDefer = $shouldDefer;
+        $this->task = $task;
+    }
+
+    public function getShouldDefer(): bool
+    {
+        return $this->shouldDefer;
+    }
+
+    public function setShouldDefer(bool $shouldDefer): void
+    {
+        $this->shouldDefer = $shouldDefer;
+    }
+
+    public function getTask(): TaskInterface
+    {
+        return $this->task;
+    }
+}
