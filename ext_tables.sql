2627a6d2e278f4fe69d6fad94e17d55936ec9677
Thorben Nissen <thorben@webcoast.dk>
2025-09-17 17:43:33 +0200

    [TASK] Add index on `checksum` for processed fields table

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,5 +1,7 @@
 # ref. https://github.com/TYPO3/typo3/blob/11.5/typo3/sysext/core/ext_tables.sql#L226
 CREATE TABLE sys_file_processedfile
 (
-    processed TINYINT(1) DEFAULT 0 NOT NULL
+    processed TINYINT(1) DEFAULT 0 NOT NULL,
+
+    KEY checksum (checksum)
 );
